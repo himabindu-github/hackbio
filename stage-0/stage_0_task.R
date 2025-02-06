8b74a099f23265721b5aa3fe9a74542b04e2b7e4
himabindu-github <99520700+himabindu-github@users.noreply.github.com>
2025-02-06 14:17:18 -0700

    Add files via upload

---
+++ stage-0/stage_0_task.R
@@ -0,0 +1,18 @@
+# stage-0 task
+
+task_stage0 <- data.frame(
+  Name =  c("Aisha Mehmood","Himabindu Kumdam", "Olabode Kaosara", "Oluwabusola Asenuga"),
+  Slack_name = c("Aisha", "Himabindu", "TheResearchNerd", "Oluwabusola"),
+  email_id = c("aishamehmood23@gmail.com", "khbiitm@gmail.com", "olabodeomowunmi5@gmail.com", "asenugaoluwabusola@gmail.com"),
+  Hobbies = c("Reading and shopping", "Music", "Research, Travelling, Fashion", "Watching vlogs, Reading Novels, Dressing up"),
+  Country_of_origin = c("Pakistan", "India", "Nigeria", "Nigeria"),
+  Background = c("Biotechnology", "Biotechnology", "Natural product chemistry", "Zoology"),
+  Programing_language = c("R", "R", "R", "Python"))
+# a dataframe "task_stage0" was created using the function data.frame()
+# The function data.frame() creates a data frame, a two dimensional array/table with rows and columns
+# Each column can contain a different data type.
+# c() is used to create a vector which is essentially a sequence of numbers or characters or other data types.
+# the vectors created are passed as arguements to data.frame(), where they are stored as columns
+
+print(task_stage0)
+# prints the output of task_stage0
